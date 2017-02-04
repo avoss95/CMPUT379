161290750c3071eaf17b30d1211e18534a8ce8f6
avoss95 <avoss@ualberta.ca>
2017-02-04 12:51:02 -0700

    still a mess, but less so

--- lab1/findpattern.c
+++ lab1/findpattern.c
@@ -16,7 +16,6 @@ struct patmatch {
   unsigned char mode; /* MEM_RW, or MEM_RO */
 };
 
-
 jmp_buf env;
 
 // global variable so that env can be used in my signal handler as well as my loop
@@ -24,8 +23,7 @@ jmp_buf env;
 unsigned int findpattern(unsigned char *pattern, unsigned int patlength, struct patmatch *locations, unsigned int loclength) {
   
   int j, pagesize, count = 0;
-  long i, max = pow(2,32);
-  //uint32_t i;
+  long long i, cap = 4294967296;
   int num_increments = 0;
   char * temp;
   int v;
@@ -35,19 +33,14 @@ unsigned int findpattern(unsigned char *pattern, unsigned int patlength, struct 
   for (j=0; j<loclength; j++) {
 
     locations[j].location = 0;
-    // no idea if this is proper syntax or not- I really need to brush up on my C pointer knowledge
-    // maybe this is why I didn't do too well on that section of the 201 final?
+    
     locations[j].mode = MEM_RO;
     // with this now done, we can safely modify the array *locations by inserting structs into the correct locations
 
   }
 
-  for (i=0; i<max; i++) {
-    
-    if (i<0)
-      {
- 	break;
-      }
+  for (i=0; i<cap; i+=1) 
+    {
     
   // i = 0;
   // do {
@@ -55,20 +48,26 @@ unsigned int findpattern(unsigned char *pattern, unsigned int patlength, struct 
      
     j = sigsetjmp(env, 1);
     
-    // put setjmp() here so that if we try to read unaccessible memory, it jumps back to here
 
-    if (j == 1) {
+    /*if (i<0)
+    	break;
+	}*/
+
+    // put sigsetjmp() here so that if we try to read unaccessible memory, it jumps back to here
+
+    if (j == 1) 
+      {
 
       pagesize = getpagesize(); 
       
       //grab the page size and increment i by that amount so we can skip the unreadable memory
       i += pagesize;
 
-    }
+      }
     
     
     
-    printf("i = %li\n", i);
+    printf("i = %lli\n", i);
 
     temp = (char *) i;
     // I can't do this because a double can't be turned into a char *; however, an int can
@@ -76,11 +75,12 @@ unsigned int findpattern(unsigned char *pattern, unsigned int patlength, struct 
 
     v = 0;
 
-    //    printf("temp = %c\n", *temp);
-
     while (*(temp+v) == pattern[v])
       // assuming I know what I'm doing, this should simply go through and increment num_increments every time the *i and *pattern match (the specific byte we're looking at)
       {
+
+	printf("pattern[v] = %c\n", pattern[v]);
+	printf("temp = %c\n", *(temp+v));
 
     	v += 1;
 
@@ -93,46 +93,54 @@ unsigned int findpattern(unsigned char *pattern, unsigned int patlength, struct 
       // hyopthetically, this should ensure that we found a match exactly equal to the pattern (not shorter or longer)
       {
 
+	printf("num_increments = %i\n", num_increments);
+	sleep(2);
+
+	//	printf("v = %i\n", v);
+
     	struct patmatch match; 
 	// create a instance of the patmatch struct to store the current match in
 
 	match.location = i; 
 	// set the location of the match to the current address
 
-	locations[count] = match;  
-	// I think ? this is (assuming this is the first match found) putting the match in the first spot in the locations array
-
-	count += 1; 
-	// can only increment after adding the match to the list since otherwise we would be off by one
-	
 	// I've started by assuming that the memory is RO
 	// try to write- if it succeeds, change to RW
 	// otherwise, leave it as is
 
-	//j = sigsetjmp(env, 1);
+	//	j = sigsetjmp(env, 1);
 	// so if we get a segfault, it will jump back here 
 
 	// crap- if I do the write here, and it segfaults, we get stuck in an infinite loop....
 
-	//	if (j == 1) {
+	if (j == 1) 
+	  {
 
 	  // if j == 1, then a segfault happened
 	  // segfault means memory is RO, otherwise it is RW
 
 	  
-	//	}
+	  }
 
 	//match.mode = MEM_RW; 
-	// I forget how to determine if the memory is RW or RO, so until I figure that out this placeholder will be here
-	//	i += v;
 
+
+	locations[count] = match;  
+	// I think ? this is (assuming this is the first match found) putting the match in the first spot in the locations array
+
+	count += 1; 
+	// can only increment after adding the match to the list since otherwise we would be off by one
+
+	//i += v;
 	// need to move on past the located pattern
 	
-	//	printf("Found a match.\n");
+	printf("Found a match.\n");
+
+	sleep(2);
 
       }
    
-  }
+    }
   
   return(count);
 
@@ -163,13 +171,19 @@ int main() {
   // call findpattern() at some point, with all the required arguments
   // pretty sure that's all main() will do- call findpattern().... I think
 
-  char * pattern = "a";
-  unsigned int patlength = 1;
+  char pattern[] = "abcdefgfcdeba";
+  unsigned int patlength = 13;
   struct patmatch locations[5];
   unsigned int loclength = 5;
 
   findpattern(pattern, patlength, locations, loclength);
 
+  /*long  long i, max = 4294967296;
+  for (i=0;i<max;i+=1024) 
+    {
+      printf("i = %lli\n", i);
+      } */
+  
   return 0;
 
 }
